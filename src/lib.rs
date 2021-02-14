32b9d519e57689c7d8b5f90cf68bd453f670f29d
eda3 <gerorin1010@gmail.com>
2021-02-14 12:04:49 +0900

    add macro measure_elapsed_time

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 mod utils;
 mod logic;
-
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::{Clamped, JsCast};
+use web_sys::ImageData;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -11,10 +12,22 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 extern "C" {
-  fn alert(s: &str);
+  #[wasm_bindgen(js_namespace = console)]
+  fn log(a: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-  alert("Hello, rust-mandelbrot!");
+macro_rules! console_log {
+  ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
+
+macro_rules! measure_elapsed_time{
+  ($t:tt,$s:block) => {{
+      let windows = web_sys::window().expect("sholud have a window the context");
+      let performace = window
+        .performance()
+        .expect("peformance should be available");
+      let start = performance.now();
+      console_log("{}:{}[ms]", $t, end - start);
+      result
+    }};
+  }
