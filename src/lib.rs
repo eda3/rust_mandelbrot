e6b53fcbd24d41dd53ee436b17f4a87ade640901
eda3 <gerorin1010@gmail.com>
2021-02-16 06:06:11 +0900

    input keyboard

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,10 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::{Clamped, JsCast};
 use web_sys::{ImageData};
 
+use color_space::*;
+use color_space::ToRgb;
+use color_space::Rgb;
+
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -46,14 +50,62 @@ pub fn generate_mandelbrot_set(
   max_iter: usize,
 ) -> Vec<u8> {
   measure_elapsed_time!("generate:wasm\telapsed:", {
-    logic::generate_mandelbrot_set(canvas_w, canvas_h,
+    generate_mandelbrot_set2(canvas_w, canvas_h,
       x_min, x_max, y_min, y_max, max_iter)
   })
 }
 
 
+fn get_n_diverged(x0: f64, y0: f64, max_iter: usize) -> u8 {
+  // 複素数z_nの実部をxn,虚部をynとします
+  let mut xn = 0.0;
+  let mut yn = 0.0;
+  for i in 1..max_iter {
+    let x_next = xn * xn - yn * yn + x0;
+    let y_next = 2.0 * xn * yn + y0;
+
+    xn = x_next;
+    yn = y_next;
+    if yn * yn + xn * xn > 4.0 {
+      return i as u8; // 複素数の絶対値が2を超えると発散と判定
+    }
+  }
+  max_iter as u8
+}
+
+
+pub fn generate_mandelbrot_set2(
+  canvas_w: usize,
+  canvas_h: usize,
+  x_min: f64,
+  x_max: f64,
+  y_min: f64,
+  y_max: f64,
+  max_iter: usize,
+) -> Vec<u8> {
+  // JSの8bit符号なし整数の配列であるUint8ClampledAllay型をつくりたいため、Vec<u8>で色情報を作る
+  let mut data = vec![];
+  for i in 0..500 {
+    for _ in 0..canvas_w {
+      // let v = (255 % j) as f64;
+      let v = i as f64;
+
+      let hsv = Hsv::new(v as f64,1.0,1.0);
+      let rgb = Rgb::from_color(&hsv);
+
+      data.push(rgb.r as u8); // R
+      data.push(rgb.g as u8); // G
+      data.push(rgb.b as u8); // B
+      data.push(255); // A
+    }
+  }
+  data
+}
+
+
 #[wasm_bindgen]
 pub fn draw_mandelbrot_set(){
+  log("A");
   // const CANVAS_ID :&str = "canvas_wasm";
   const CANVAS_ID :&str = "canvas_hybrid";
   let document = web_sys::window().unwrap().document().unwrap();
@@ -79,7 +131,7 @@ pub fn draw_mandelbrot_set(){
   const MAX_ITER: usize = 64;
 
   let mut result = measure_elapsed_time!("generate:wasm\telapsed:",{
-    logic::generate_mandelbrot_set(canvas_w, canvas_h,
+    generate_mandelbrot_set2(canvas_w, canvas_h,
       X_MIN, X_MAX, Y_MIN, Y_MAX, MAX_ITER)
   });
   measure_elapsed_time!("generate:wasm\telapsed:",{
--- src/logic.rs
+++ src/logic.rs
@@ -1,49 +1,8 @@
-fn get_n_diverged(x0: f64, y0: f64, max_iter: usize) -> u8 {
-  // 複素数z_nの実部をxn,虚部をynとします
-  let mut xn = 0.0;
-  let mut yn = 0.0;
-  for i in 1..max_iter {
-    let x_next = xn * xn - yn * yn + x0;
-    let y_next = 2.0 * xn * yn + y0;
+use color_space::*;
+use color_space::ToRgb;
+use color_space::Rgb;
 
-    xn = x_next;
-    yn = y_next;
-    if yn * yn + xn * xn > 4.0 {
-      return i as u8; // 複素数の絶対値が2を超えると発散と判定
-    }
-  }
-  max_iter as u8
-}
 
-pub fn generate_mandelbrot_set(
-  canvas_w: usize,
-  canvas_h: usize,
-  x_min: f64,
-  x_max: f64,
-  y_min: f64,
-  y_max: f64,
-  max_iter: usize,
-) -> Vec<u8> {
-  let canvas_w_f64 = canvas_w as f64;
-  let canvas_h_f64 = canvas_h as f64;
-  // JSの8bit符号なし整数の配列であるUint8ClampledAllay型をつくりたいため、Vec<u8>で色情報を作る
-  let mut data = vec![];
-  for i in 0..canvas_h {
-    let i_f64 = i as f64;
-    let y = y_min + (y_max - y_min) * i_f64 / canvas_h_f64;
-    for j in 0..canvas_w {
-      let x = x_min + (x_max - x_min) * j as f64 / canvas_w_f64;
-      let iter_index = get_n_diverged(x, y, max_iter);
-      let v = iter_index % 8 * 32; // 8色に塗り分ける
-      println!("{:?}",v);
-      data.push(v); // R
-      data.push(v); // G
-      data.push(v); // B
-      data.push(255); // A
-    }
-  }
-  data
-}
 #[cfg(test)]
 mod tests {
   use super::*;
